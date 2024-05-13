5587965d0a4fa145826275e94b825925f7f45602
artur barroso <165661304+artur-barroso@users.noreply.github.com>
2024-05-13 07:35:04 -0300

    3 botões
    mais uma atividade pronta

---
+++ desafio02/codigo.js
@@ -0,0 +1,9 @@
+function valor1() {
+    alert('Você apertou o botão 1.')
+}
+function valor2() {
+    alert('Você apertou o botão 2.')
+}
+function valor3() {
+    alert('Você apertou o botão 3.')
+}
