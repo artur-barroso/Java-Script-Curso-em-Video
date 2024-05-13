fa7f45fa60b799a750e0bc979167befa59eedfbb
artur barroso <165661304+artur-barroso@users.noreply.github.com>
2024-05-13 09:05:44 -0300

    desafio03
    mais um desafio feito

---
+++ desafio03/comandos.js
@@ -0,0 +1,28 @@
+function notas() {
+    var nome = prompt('Nome do aluno.')
+    var nota1 = prompt(`Primeira nota de ${nome}`)
+    var nota2 = prompt(`Segunda nota de ${nome}`)
+    var n1 = Number(nota1)
+    var n2 = Number(nota2)
+    if (nota1 == "" || nota2 == "") {
+        alert('Digite uma nota valida.')
+    } else {
+        let res = document.getElementById('res')
+        let m = (n1 + n2) / 2
+        let media = Number(m)
+        if (media > 6) {
+            sit.style.color = "green";
+            res.innerHTML = `A média de ${nome} é ${media}: `
+            sit.innerHTML = 'APROVADO.'
+        } else if (media>3 && media <6){
+            sit.style.color = "yellow";
+            res.innerHTML = `A média de ${nome} é ${media}`
+            sit.innerHTML = "RECUPERAÇÃO"
+        } else {
+            sit.style.color = "red";
+            res.innerHTML = `A média de ${nome} é ${media}` 
+            sit.innerHTML = 'REPROVADO.'
+            
+        }
+    }
+}
