bff5428f6eae6ed410318c82798999e1679f338c
artur barroso <165661304+artur-barroso@users.noreply.github.com>
2024-05-15 12:01:01 -0300

    mudei alguns programas

--- desafio13/comandos.js
+++ desafio13/comandos.js
@@ -4,25 +4,22 @@ function notas() {
     var nota2 = prompt(`Segunda nota de ${nome}`)
     var n1 = Number(nota1)
     var n2 = Number(nota2)
-    var re = sit.innerHTML = "Reprovado"
     if (nota1 == "" || nota2 == "") {
         alert('Digite uma nota valida.')
     } else {
         let res = document.getElementById('res')
         let m = (n1 + n2) / 2
         let media = Number(m)
+        let n = document.getElementById('nada')
+        n.innerHTML = ""
+        res.innerHTML = ""
         if (media > 6) {
-            sit.style.color = "green";
-            res.innerHTML = `A média de ${nome} é ${media}: `
-            sit.innerHTML = 'APROVADO.'
+            res.innerHTML = `A média de ${nome} é ${media}: <strong class="g"> APROVADO</strong>`
         } else if (media>3 && media <6){
-            sit.style.color = "yellow";
-            res.innerHTML = `A média de ${nome} é ${media}`
-            sit.innerHTML = "RECUPERAÇÃO"
+            res.innerHTML = `A média de ${nome} é ${media}:  <strong class="y"> RECUPERÇÃO </strong>`
         } else {
-            sit.style.color = "red";
-            res.innerHTML = `A média de ${nome} é ${media}` 
-            sit.innerHTML = 'REPROVADO.'
+            res.innerHTML = `A média de ${nome} é ${media}: <strong class="r"> REPROVADO </strong>` 
+
             
         }
     }
